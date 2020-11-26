afb98cde36309cc13206c772516a6d26cda7bb56
rdalvarez43 <rdalvarez43@gmail.com>
2020-11-26 09:12:00 +0100

    Listo para pull

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const bodyParser = require("body-parser");
 const corsEnable = require("cors");
 const cookieParser = require("cookie-parser");
 const validatorNode = require("./lib/validatorMoviesNode.class.js");
+const fetch = require('node-fetch');
+
 //const JWT = require("./lib/jwt.js");
 
 //Creation of Express server
@@ -14,6 +16,7 @@ const serverObj = express();
 //Express server setup
 //Definition of listening port
 const listeningPort = 8888;
+
 
 //------------------ MIDDLEWARES -------------------//
 //Setup the public (Frontend) folder
@@ -111,3 +114,79 @@ serverObj.post("/login", (req, res) => {
 serverObj.post("/logout", (req, res) => {
 	//TODO
 });
+
+//OAUTH
+const google= require('googleapis');
+//import { google } from 'googleapis';
+
+let GOOGLE_CLIENT_SECRET="hgQOLqZ2MPLH-r0B9Glq5TdW";
+let GOOGLE_CLIENT_ID = "http://892702418247-bgj3ovrtauoh0i2ru4qs0j9tbg1rn1ma.apps.googleusercontent.com/"
+
+const oauth2Client = new google.auth.OAuth2(
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  /*
+   * This is where Google will redirect the user after they
+   * give permission to your application
+   */
+  `http://localhost:8080`,
+);
+
+function getGoogleAuthURL() {
+    /*
+     * Generate a url that asks permissions to the user's email and profile
+     */
+    const scopes = [
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email',
+    ];
+
+    return oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      prompt: 'consent',
+      scope: scopes, // If you only need one scope you can pass it as string
+    });
+}
+
+async function getGoogleUser({ code }) {
+  const { tokens } = await oauth2Client.getToken(code);
+
+  oauth2Client.setCredentials(tokens);
+
+  // Fetch the user's profile with the access token and bearer
+  const googleUser = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${tokens.access_token}`, {
+	  headers: {
+          Authorization: `Bearer ${tokens.id_token}`
+	  }
+  })
+    .then(res => res.data)
+    .catch(error => {
+      throw new Error(error.message);
+    });
+
+  return googleUser;
+}
+/*
+async googleAuth(input, context) {
+	const googleUser = await getGoogleUser({ code: input.code });
+
+    let user = await this.userModel
+      .findOne({ githubId: String(googleUser.id) })
+      .exec();
+
+    if (user) {
+      // Update their profile
+
+    }
+
+    if (!user) {
+      // Create the user in the database
+      user = new User()
+    }
+
+    // Generate a JWT, add it as a cookie
+
+    return user;
+}
+*/
+
