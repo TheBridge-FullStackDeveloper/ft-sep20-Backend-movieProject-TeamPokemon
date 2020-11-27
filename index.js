c876b3ebc75e8d9dbd4ecc78cc176a525338ba24
Tomas <tomas.vila.sanchez@gmail.com>
2020-11-27 09:17:07 +0100

    ready to pull

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const MongoClient = require("mongodb").MongoClient;
 const uri = "mongodb+srv://PokemonTeam:5pokemon@pokemon.afmh3.mongodb.net?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { "useNewUrlParser": true, "useUnifiedTopology": true });
 client.connect(err => {
-	const collection = client.db("test").collection("devices");
+	//const collection = client.db("test").collection("devices");
 	// perform actions on the collection object
 	client.close();
 });
